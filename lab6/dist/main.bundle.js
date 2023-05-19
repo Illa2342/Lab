fdfb36e0c9f45cc0f3b73596f2f3bcd3e6a32bfa
Илля <illadrobnij@gmail.com>
2023-05-19 23:42:36 +0300

    Lab_6

---
+++ lab6/dist/main.bundle.js
@@ -0,0 +1,149 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/styles/fonts.scss":
+/*!*******************************!*\
+  !*** ./src/styles/fonts.scss ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/fonts.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/gallery.scss":
+/*!*********************************!*\
+  !*** ./src/styles/gallery.scss ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/gallery.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/gradient.scss":
+/*!**********************************!*\
+  !*** ./src/styles/gradient.scss ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/gradient.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/nav.scss":
+/*!*****************************!*\
+  !*** ./src/styles/nav.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/nav.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/news.scss":
+/*!******************************!*\
+  !*** ./src/styles/news.scss ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/news.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/normalize.css":
+/*!**********************************!*\
+  !*** ./src/styles/normalize.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/normalize.css?");
+
+/***/ }),
+
+/***/ "./src/styles/position.scss":
+/*!**********************************!*\
+  !*** ./src/styles/position.scss ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/position.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/table.scss":
+/*!*******************************!*\
+  !*** ./src/styles/table.scss ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/styles/table.scss?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_fonts_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/fonts.scss */ \"./src/styles/fonts.scss\");\n/* harmony import */ var _styles_gallery_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/gallery.scss */ \"./src/styles/gallery.scss\");\n/* harmony import */ var _styles_gradient_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/gradient.scss */ \"./src/styles/gradient.scss\");\n/* harmony import */ var _styles_nav_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./styles/nav.scss */ \"./src/styles/nav.scss\");\n/* harmony import */ var _styles_news_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./styles/news.scss */ \"./src/styles/news.scss\");\n/* harmony import */ var _styles_normalize_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./styles/normalize.css */ \"./src/styles/normalize.css\");\n/* harmony import */ var _styles_position_scss__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./styles/position.scss */ \"./src/styles/position.scss\");\n/* harmony import */ var _styles_table_scss__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./styles/table.scss */ \"./src/styles/table.scss\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
